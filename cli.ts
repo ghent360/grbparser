d590cf6f87925e8294317f9e55ec293164a1b9c5
Venelin Efremov <ghent360@iqury.us>
2018-06-09 19:41:53 -0700

    Add more data to the mega test suite.

--- cli.ts
+++ cli.ts
@@ -130,28 +130,74 @@ function percent(n:number):string {
     return (n * 100).toFixed(2) + "%";
 }
 
+function fixed(n:number, pos:number):string {
+    let result = n.toFixed(0);
+    while (result.length < pos) {
+        result = '0' + result;
+    }
+    return result;
+}
+
+function formatTime(millis:number):string {
+    return fixed(millis / 1000 / 60, 2) + ':'
+        + fixed((millis / 1000) % 60, 2) + '.'
+        + fixed(millis % 1000, 3);
+}
+
 function analyze(result:Array<any>):any {
     let totalCount = result.length;
-    let successCount = result.filter(r => r.status == "success").length;
-    let parseErrorCount = result.filter(r => r.status == "parse error").length;
+    let successResults = result.filter(r => r.status == "success");
+    let successCount = successResults.length;
+    let successGerber = successResults.filter(r => r.type == "Gerber");
+    let successExcellon = successResults.filter(r => r.type == "Excellon");
+    let successCountGerber = successGerber.length;
+    let successCountExcellon = successExcellon.length;
+    let totalDuration = successResults.map(r => r.duration).reduce((a, r) => a += r);
+    let totalDurationGerber = successGerber.map(r => r.duration).reduce((a, r) => a += r);
+    let totalDurationExcellon = successExcellon.map(r => r.duration).reduce((a, r) => a += r);
+    let slowestGerber = successGerber.sort((a, b) => b.duration - a.duration)[0];
+    let slowestExcellon = successExcellon.sort((a, b) => b.duration - a.duration)[0];
+    let perseError = result.filter(r => r.status == "parse error");
+    let parseErrorCount = perseError.length;
+    let parseErrorCountGerber = perseError.filter(r => r.type == "Gerber").length;
+    let parseErrorCountExcellon = perseError.filter(r => r.type == "Excellon").length;
     let ioErrorCount = result.filter(r => r.status == "io error").length;
     let unzipErrorCount = result.filter(r => r.status == "unzip error").length;
     let skipCount = result.filter(r => r.status == "skip").length;
-    let gerbersTried = totalCount - skipCount - unzipErrorCount - ioErrorCount;
+    let filesTried = totalCount - skipCount - unzipErrorCount - ioErrorCount;
     console.log(`Total results ${totalCount}`);
     console.log(`Skipped       ${skipCount}\t${percent(skipCount / totalCount)}`);
-    console.log(`Success       ${successCount}\t${percent(successCount / gerbersTried)}`);
-    console.log(`Parse Error   ${parseErrorCount}\t${percent(parseErrorCount / gerbersTried)}`);
+    console.log(`Success       ${successCount}\t${percent(successCount / filesTried)}`);
+    console.log(`    Gerber    ${successCountGerber}`);
+    console.log(`    Excellon  ${successCountExcellon}`);
+    console.log(`Parse Error   ${parseErrorCount}\t${percent(parseErrorCount / filesTried)}`);
+    console.log(`    Gerber    ${parseErrorCountGerber}`);
+    console.log(`    Excellon  ${parseErrorCountExcellon}`);
+    console.log(`Total time    ${formatTime(totalDuration)}`);
+    console.log(`    Gerber    ${formatTime(totalDurationGerber)}`);
+    console.log(`    Excellon  ${formatTime(totalDurationExcellon)}`);
+    console.log('Slow pokes');
+    console.log(`    Gerber    ${formatTime(slowestGerber.duration)}\t${slowestGerber.zipFileName}\t${slowestGerber.file}`);
+    console.log(`    Excellon  ${formatTime(slowestExcellon.duration)}\t${slowestExcellon.zipFileName}\t${slowestExcellon.file}`);
     console.log(`I/O Error     ${ioErrorCount}`);
     console.log(`Unzip Error   ${unzipErrorCount}`);
     return {
         totalCount:totalCount,
         successCount:successCount,
+        successCountGerber:successCountGerber,
+        successCountExcellon:successCountExcellon,
         parseErrorCount:parseErrorCount,
+        parseErrorCountGerber:parseErrorCountGerber,
+        parseErrorCountExcellon:parseErrorCountExcellon,
+        totalDuration:totalDuration,
+        totalDurationGerber:totalDurationGerber,
+        totalDurationExcellon:totalDurationExcellon,
+        slowestGerber:slowestGerber,
+        slowestExcellon:slowestExcellon,
         ioErrorCount:ioErrorCount,
         unzipErrorCount:unzipErrorCount,
         skipCount:skipCount,
-        gerbersTried:gerbersTried
+        filesTried:filesTried
     };
 }
 
@@ -160,8 +206,15 @@ function compare(oldResult:any, newResult:any) {
     console.log('New and old comparison:');
     console.log(`Total results ${oldResult.totalCount}\t\t\t${newResult.totalCount}`);
     console.log(`Skipped       ${oldResult.skipCount}\t${percent(oldResult.skipCount / oldResult.totalCount)}\t\t${newResult.skipCount}\t${percent(newResult.skipCount / newResult.totalCount)}`);
-    console.log(`Success       ${oldResult.successCount}\t${percent(oldResult.successCount / oldResult.gerbersTried)}\t\t${newResult.successCount}\t${percent(newResult.successCount / newResult.gerbersTried)}`);
-    console.log(`Parse Error   ${oldResult.parseErrorCount}\t${percent(oldResult.parseErrorCount / oldResult.gerbersTried)}\t\t${newResult.parseErrorCount}\t${percent(newResult.parseErrorCount / newResult.gerbersTried)}`);
+    console.log(`Success       ${oldResult.successCount}\t${percent(oldResult.successCount / oldResult.filesTried)}\t\t${newResult.successCount}\t${percent(newResult.successCount / newResult.filesTried)}`);
+    console.log(`  Gerber      ${oldResult.successCountGerber}\t \t\t${newResult.successCountGerber}`);
+    console.log(`  Excellon    ${oldResult.successCountExcellon}\t \t\t${newResult.successCountExcellon}`);
+    console.log(`Parse Error   ${oldResult.parseErrorCount}\t${percent(oldResult.parseErrorCount / oldResult.filesTried)}\t\t${newResult.parseErrorCount}\t${percent(newResult.parseErrorCount / newResult.filesTried)}`);
+    console.log(`  Gerber      ${oldResult.parseErrorCountGerber}\t \t\t${newResult.parseErrorCountGerber}`);
+    console.log(`  Excellon    ${oldResult.parseErrorCountExcellon}\t \t\t${newResult.parseErrorCountExcellon}`);
+    console.log(`Total time    ${formatTime(oldResult.totalDuration)}\t \t\t${formatTime(newResult.totalDuration)}`);
+    console.log(`  Gerber      ${formatTime(oldResult.totalDurationGerber)}\t \t\t${formatTime(newResult.totalDurationGerber)}`);
+    console.log(`  Excellon    ${formatTime(oldResult.totalDurationExcellon)}\t \t\t${formatTime(newResult.totalDurationExcellon)}`);
     console.log(`I/O Error     ${oldResult.ioErrorCount}\t\t\t\t${newResult.ioErrorCount}`);
     console.log(`Unzip Error   ${oldResult.unzipErrorCount}\t\t\t\t${newResult.unzipErrorCount}`);
 }
